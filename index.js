8369a2fef253a27eeb8eda67a48144025d8e20b8
RisbourgNathan <nathan.risbourg@ynov.com>
2020-01-08 19:57:06 +0100

    Add .babelrc

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ import { resourceRoutes } from "./src/routes/resourceRoutes";
 import { projectRoutes } from "./src/routes/ProjectRoutes";
 import { serviceRoutes } from "./src/routes/serviceRoutes";
 
-const PORT = 3000;
+const PORT = 3200;
+
+
+const socket = require('socket.io-client');
+let client = socket.connect('http://51.15.137.122:18000/', {reconnect: true}); // L'adresse IP vous sera communiqué dans un mail ultérieur
+
+client.on('connect', () => {
+  console.log('connected');
+
+  client.emit('getServices');
+  client.on('servicies', (data) => console.log(data));
+
+//   client.emit('needHelp');
+//   client.on('info', (data) => console.log(data));
+
+
+});
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
